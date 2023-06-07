c0fb8135312fb1583892abcd1643a668815dfada
Bas van Dinther <bas@vormkracht10.nl>
2023-06-07 11:14:41 +0200

    Add missing methods

--- src/WeFact.php
+++ src/WeFact.php
@@ -5,6 +5,8 @@ namespace Vormkracht10\WeFact;
 use GuzzleHttp\Client;
 use Vormkracht10\WeFact\Resources\Debtor;
 use Vormkracht10\WeFact\Resources\Invoice;
+use Vormkracht10\WeFact\Resources\Group;
+use Vormkracht10\WeFact\Resources\Product;
 
 class WeFact
 {
@@ -49,6 +51,24 @@ class WeFact
     public function debtor(): Debtor
     {
         return new Debtor(
+            $this->http,
+            $this->apiKey,
+            $this->apiUrl
+        );
+    }
+
+    public function group(): Group
+    {
+        return new Group(
+            $this->http,
+            $this->apiKey,
+            $this->apiUrl
+        );
+    }
+
+    public function product(): Product
+    {
+        return new Product(
             $this->http,
             $this->apiKey,
             $this->apiUrl
