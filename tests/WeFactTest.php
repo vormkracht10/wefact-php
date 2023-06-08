cdec2a46fa61d702e6a7ffe85b886765c85720c2
Bas van Dinther <bas@vormkracht10.nl>
2023-06-08 13:26:50 +0200

    Fix tests

--- tests/WeFactTest.php
+++ tests/WeFactTest.php
@@ -1,10 +1,14 @@
 <?php
 
-use Vormkracht10\WeFact\Resources\Debtor;
+use Vormkracht10\WeFact\WeFact;
 use Vormkracht10\WeFact\Resources\Group;
+use Vormkracht10\WeFact\Resources\Debtor;
 use Vormkracht10\WeFact\Resources\Invoice;
 use Vormkracht10\WeFact\Resources\Product;
-use Vormkracht10\WeFact\WeFact;
+use Vormkracht10\WeFact\Resources\Subscription;
+use Vormkracht10\WeFact\Resources\Settings\Settings;
+use Vormkracht10\WeFact\Resources\Settings\CostCategory;
+use Vormkracht10\WeFact\Exceptions\InvalidRequestException;
 
 it('can create wefact instance', function () {
     $apiKey = 'FAKE_API_KEY';
@@ -15,15 +19,18 @@ it('can create wefact instance', function () {
 
     expect($weFact)->toBeInstanceOf(WeFact::class);
     expect($weFact->getApiUrl())->toBe($apiUrl);
-    expect($weFact->invoice())->toBeInstanceOf(Invoice::class);
-    expect($weFact->debtor())->toBeInstanceOf(Debtor::class);
-    expect($weFact->group())->toBeInstanceOf(Group::class);
-    expect($weFact->product())->toBeInstanceOf(Product::class);
+    expect($weFact->invoices())->toBeInstanceOf(Invoice::class);
+    expect($weFact->debtors())->toBeInstanceOf(Debtor::class);
+    expect($weFact->groups())->toBeInstanceOf(Group::class);
+    expect($weFact->products())->toBeInstanceOf(Product::class);
+    expect($weFact->settings())->toBeInstanceOf(Settings::class);
+    expect($weFact->costCategories())->toBeInstanceOf(CostCategory::class);
+    expect($weFact->subscriptions())->toBeInstanceOf(Subscription::class);
 });
 
 it('can return an error when non existing API key is used', function () {
     $apiKey = 'FAKE_API_KEY';
     $weFact = new WeFact($apiKey);
 
-    $weFact->invoice()->list();
+    $weFact->invoices()->list();
 })->throws(\Vormkracht10\WeFact\Exceptions\InvalidRequestException::class);
